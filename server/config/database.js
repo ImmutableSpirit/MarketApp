de3dbdf987524de7a153a3b1f45b833218828727
immutablespirit <oa227r@gmail.com>
2024-10-05 23:39:39 -0500

    Make sure your .env file is in the server directory instead of being one level above

--- server/config/database.js
+++ server/config/database.js
@@ -1,5 +1,15 @@
-const { Sequelize } = require('sequelize');
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+console.log('Current working directory:', process.cwd());
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
+process.env.TEST_VAR = 'This is a test';
+console.log('TEST_VAR:', process.env.TEST_VAR);
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
